26fbca2721ef34c53b70cd6dda2a7b06e7aba9e5
ngocthat <ntsh9015@gmail.com>
2025-01-11 15:48:21 +0700

    update page home web

--- resources/views/frontend/page/home.blade.php
+++ resources/views/frontend/page/home.blade.php
@@ -49,7 +49,7 @@
                 <div>{{ getSetting('description_about_us') ?? '' }}</div>
             </div>
             <div class="col-lg-5 col-12" style="height: 250px; overflow: hidden">
-                <div id="carouselId" class="carousel slide position-relative" data-bs-ride="carousel">
+                <div id="carouselExample" class="carousel slide position-relative" data-bs-ride="carousel">
                     <div class="carousel-inner" role="listbox">
                         @foreach($categories as $key => $category)
                             <div class="carousel-item rounded {{ $key === 0 ? 'active' : '' }}">
@@ -58,6 +58,14 @@
                             </div>
                         @endforeach
                     </div>
+                    <button style="background: transparent" class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button style="background: transparent" class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
                 </div>
             </div>
         </div>
